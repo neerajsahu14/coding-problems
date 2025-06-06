44ea5a074ce6596645ebdbd6aeaaf147f5bb47ed
Neeraj Sahu <142705889+neerajsahu14@users.noreply.github.com>
2025-06-06 22:06:34 +0530

    Time: 0 ms (100%), Space: 10 MB (5.25%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -5,16 +5,33 @@ public:
         if(n%2==1) return false;
         stack<int> st;
         for(auto ch : s){
-            if(ch =='(' ||ch =='[' || ch =='{' )
-                st.push(ch);
-            else{
-                if(st.empty()) return false;
-                char top = st.top();
-                st.pop();
-                if(( ch ==')' && top !='(') ||
-                    ( ch =='}' && top !='{') ||
-                    ( ch ==']' && top !='[')
-                ) return false;
+            // if(ch =='(' ||ch =='[' || ch =='{' )
+            //     st.push(ch);
+            // else{
+            //     if(st.empty()) return false;
+            //     char top = st.top();
+            //     st.pop();
+            //     if(( ch ==')' && top !='(') ||
+            //         ( ch =='}' && top !='{') ||
+            //         ( ch ==']' && top !='[')
+            //     ) return false;
+            // }
+            switch(ch){
+                case '{' : case '[' : case '(' :
+                    st.push(ch);
+                    break;
+                case ')':
+                    if (st.empty() || st.top() != '(') return false;
+                    st.pop();
+                    break;
+                case '}':
+                    if (st.empty() || st.top() != '{') return false;
+                    st.pop();
+                    break;
+                case ']':
+                    if (st.empty() || st.top() != '[') return false;
+                    st.pop();
+                    break;   
             }    
         }
         return st.empty();

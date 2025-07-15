71cfcf0cf70307e4448cb6043ec83548a8eb02e7
Neeraj Sahu <142705889+neerajsahu14@users.noreply.github.com>
2025-07-15 22:07:52 +0530

    Time: 0 ms (100%), Space: 10.8 MB (58.09%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,25 +1,88 @@
+// class Solution {
+// public:
+//     bool exist(vector<vector<char>>& board, string word) {
+//         int row = board.size();
+//         int col = board[0].size();
+//         vector<vector<bool>> visited(row, vector<bool>(col, false));
+//         function<bool(int, int, int)> dfs = [&](int r, int c, int i) {
+//             if(i==word.size()) return true;
+//             if(r<0 || c<0 || r>=row ||
+//              c>=col || word[i]!=board[r][c] || visited[r][c])
+//              return false;
+//              visited[r][c]=true;
+//              bool res = dfs(r+1,c,i+1) || dfs(r-1,c,i+1) || dfs(r,c+1,i+1) || dfs(r,c-1,i+1);
+//              visited[r][c]=false;
+//              return res;
+//         };
+//         for(int r=0;r<row;r++){
+//             for(int c=0; c<col; c++){
+//                 if(dfs(r,c,0)) return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        int row = board.size();
-        int col = board[0].size();
-        vector<vector<bool>> visited(row, vector<bool>(col, false));
-        function<bool(int, int, int)> dfs = [&](int r, int c, int i) {
-            if(i==word.size()) return true;
-            if(r<0 || c<0 || r>=row ||
-             c>=col || word[i]!=board[r][c] || visited[r][c])
-             return false;
-             visited[r][c]=true;
-             bool res = dfs(r+1,c,i+1) || dfs(r-1,c,i+1) || dfs(r,c+1,i+1) || dfs(r,c-1,i+1);
-             visited[r][c]=false;
-             return res;
-        };
+       int n = board.size(); 
+        int m = board[0].size();
         
-        for(int r=0;r<row;r++){
-            for(int c=0; c<col; c++){
-                if(dfs(r,c,0)) return true;
+        vector<vector<bool>> visited(n, vector<bool>(m, false)); 
+        vector<char> wordChar(word.begin(), word.end());
+        
+        if (wordChar.size() > n * m)
+            return false;
+        vector<int> counts(256, 0); 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                counts[board[i][j]]++;
             }
         }
-        return false;
+        int len = wordChar.size();
+        for (int i = 0; i < len / 2; i++) {
+            if (counts[wordChar[i]] > counts[wordChar[len - 1 - i]]) {
+                for (int j = 0; j < len / 2; j++) {
+                    char temp = wordChar[j];
+                    wordChar[j] = wordChar[len - 1 - j];
+                    wordChar[len - 1 - j] = temp;
+                }
+                break;
+            }
+        }
+        
+        for (char c : wordChar) {
+            if (--counts[c] < 0)
+                return false;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (visit(board, wordChar, 0, i, j, n, m, visited))
+                    return true;
+            }
+        }
+        return false; 
+    }
+    bool visit(vector<vector<char>>& board, vector<char>& word, int start, int x, int y,
+            int n, int m, vector<vector<bool>>& visited) {
+        if (start == word.size())
+            return true;
+        
+        if (x < 0 || x >= n || y < 0 || y >= m || visited[x][y])
+            return false;
+        
+        if (word[start] != board[x][y])
+            return false;
+        
+        visited[x][y] = true; 
+        
+        bool found = visit(board, word, start + 1, x + 1, y, n, m, visited)
+                || visit(board, word, start + 1, x - 1, y, n, m, visited)
+                || visit(board, word, start + 1, x, y + 1, n, m, visited)
+                || visit(board, word, start + 1, x, y - 1, n, m, visited);
+        
+        visited[x][y] = false; 
+        return found;
     }
 };

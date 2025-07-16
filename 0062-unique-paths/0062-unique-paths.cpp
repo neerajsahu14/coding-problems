cad54a998c39af0990e531cc197e306311ec6d00
Neeraj Sahu <142705889+neerajsahu14@users.noreply.github.com>
2025-07-16 21:00:24 +0530

    Time: 0 ms (100%), Space: 9.4 MB (8.8%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -16,21 +16,16 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         // return solve(m,n,1,1);
-        // dp
-        // vector<vector<int>> dp(m,vector<int>(n,1));
-        // for(int i=1; i<m; i++){
-        //     for(int j=1; j<n;j++){
-        //         dp[i][j]=dp[i-1][j]+dp[i][j-1];
-        //     }
-        // }
-        // return dp[m-1][n-1];
-        // optimal
-        vector<int> dp(n,1);
-        for(int i=1; i<m;i++){
-            for(int j=1; j<n; j++){
-                dp[j]+=dp[j-1];
+        vector<vector<int>> dp(m,vector<int>(n,1));
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
             }
         }
-    return dp[n-1];
+        return dp[m-1][n-1];
     }
+
+    
+
+
 };

22057491ab47e67962aceb000e0da359d83415fa
Neeraj Sahu <142705889+neerajsahu14@users.noreply.github.com>
2025-07-29 06:58:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Mirror Tree/mirror-tree.cpp
@@ -0,0 +1,26 @@
+/*
+class Node {
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int x) {
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node* node) {
+        // code here
+        if(node==nullptr) return;
+        
+        swap(node->left,node->right);
+        mirror(node->left);
+        mirror(node->right);
+    }
+};

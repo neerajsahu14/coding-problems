e8b4f4ffffed95be5b4c8f8d910af6401e6edb77
Neeraj Sahu <142705889+neerajsahu14@users.noreply.github.com>
2025-08-12 07:40:20 +0530

    Time: 7 ms (22.07%), Space: 12 MB (64.88%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -21,23 +21,46 @@ public:
 
 class Solution {
 
-    unordered_map<Node*,Node*> oldToNew;
-    Node* dfs(Node* node){
-        if(oldToNew.count(node)){
+    unordered_map<Node*, Node*> oldToNew;
+
+    Node* dfs(Node* node) {
+        if (oldToNew.count(node)) {
             return oldToNew[node];
         }
         Node* copy = new Node(node->val);
         oldToNew[node] = copy;
-        for(auto nig : node->neighbors){
+        for (auto nig : node->neighbors) {
             copy->neighbors.push_back(dfs(nig));
         }
         return copy;
     }
 
+    Node* bfs(Node* node) {
+        queue<Node*> q;
+        Node* headCopy = new Node(node->val);
+        oldToNew[node] = headCopy;
+        q.push(node);
+        while (!q.empty()) {
+            Node* n = q.front();
+            q.pop();
+            for (auto nig : n->neighbors) {
+
+                if (!oldToNew.count(nig)) {
+                    Node* copy = new Node(nig->val);
+                    oldToNew[nig] = copy;
+                    q.push(nig);   
+                }
+                oldToNew[n]->neighbors.push_back(oldToNew[nig]);
+            }
+        }
+        return headCopy;
+    }
+
 public:
     Node* cloneGraph(Node* node) {
-        if(!node) return node;
+        if (!node)
+            return node;
 
-        return dfs(node);
+        return bfs(node);
     }
 };
